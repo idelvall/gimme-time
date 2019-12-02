39f6ae76f6dee64b118b378303ac728c0a62a54a
nacho <idelvall@brutusin.org>
2019-12-02 17:28:13 +0100

    add

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 npm install -g serverless
-serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r eu-west-1
+
+serverless deploy --stage $env --package target/$env -v -r eu-west-1
