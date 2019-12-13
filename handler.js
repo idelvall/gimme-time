e182432a9eb0fbdf0e1fcbd7441d39db75a8d813
nacho <idelvall@brutusin.org>
2019-12-13 17:56:13 +0100

    add agent

--- handler.js
+++ handler.js
@@ -1,24 +1,26 @@
 const moment = require('moment-timezone');
 const agent = require('@shiftleft/agent').default;
 
-new agent().startAgent(() => {
-  module.exports.gimmetime = async (event) => {
-    let tz = 'Europe/Rome';
-
-    if (event.queryStringParameters && event.queryStringParameters.tz) {
-      tz = event.queryStringParameters.tz;
-
-      if (!moment.tz.names().includes(tz)) {
-        return {
-          statusCode: '400',
-          body: `Unknown timezone ${tz}`
-        };
-      }
-    }
-
-    return {
-      statusCode: '200',
-      body: `The time in ${tz} is: ${moment.tz(tz).format()}`
-    };
-  };
+const ready = new Promise((resolve) => {
+  new agent().startAgent(() => {
+    resolve();
+  });
 });
+
+module.exports.gimmetime = async (event) => {
+  await ready;
+  let tz = 'Europe/Rome';
+  if (event.queryStringParameters && event.queryStringParameters.tz) {
+    tz = event.queryStringParameters.tz;
+    if (!moment.tz.names().includes(tz)) {
+      return {
+        statusCode: '400',
+        body: `Unknown timezone ${tz}`
+      };
+    }
+  }
+  return {
+    statusCode: '200',
+    body: `The time in ${tz} is: ${moment.tz(tz).format()}`
+  };
+};
